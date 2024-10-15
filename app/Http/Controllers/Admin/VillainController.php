76b2df75431d2bea16b810cf476acf7c3ee4ff51
Francescosommellaa <franc.sommella@gmail.com>
2024-10-15 12:14:44 +0200

    restored model

--- app/Http/Controllers/Admin/VillainController.php
+++ app/Http/Controllers/Admin/VillainController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Villain;
+use App\Models\Universe;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class VillainController extends Controller
@@ -20,7 +23,15 @@ class VillainController extends Controller
      */
     public function create()
     {
-        //
+        $userVillain = Villain::where('user_id', Auth::id())->first();
+
+        if ($userVillain) {
+            return redirect()->route('admin.villains.index')->with('error', 'Sei già un Villain.');
+        }
+
+        $universes = Universe::all();
+
+        return view('admin.villains.create', compact('universes'));
     }
 
     /**
@@ -28,7 +39,39 @@ class VillainController extends Controller
      */
     public function store(Request $request)
     {
-        //
+
+        $userVillain = Villain::where('user_id', Auth::id())->first();
+
+        if ($userVillain) {
+            return redirect()->route('admin.villains.index')->with('error', 'Sei già un Villain.');
+        }
+
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'slug' => 'required|string|max:255|unique:villains,slug',
+            'email_contact' => 'nullable|email|max:255',
+            'image' => 'nullable|image',
+            'phone' => 'nullable|string|max:20',
+            'universe_id' => 'required|exists:universes,id'
+        ]);
+
+        // Salvataggio dell'immagine se presente
+        $imagePath = null;
+        if ($request->hasFile('image')) {
+            $imagePath = $request->file('image')->store('villains', 'public');
+        }
+
+        // Crea il Villain
+        Villain::create([
+            'name' => $request->input('name'),
+            'slug' => $request->input('slug'),
+            'email_contact' => $request->input('email'),
+            'phone' => $request->input('phone'),
+            'universe_id' => $request->input('universe_id'),
+            'image' => $imagePath
+        ]);
+
+        return redirect()->route('admin.villains.index')->with('success', 'Benvenuto ora sei un vero Villain!');
     }
 
     /**
