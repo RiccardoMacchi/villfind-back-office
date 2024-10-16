9bb8bd9b90081d05ca449ca275385fb2baa5803a
Jacopo Scorretti <jacoposcorretti.99@gmail.com>
2024-10-16 15:32:44 +0200

    fix(layouts): top navigation bar logo displaying

--- resources/views/admin/partials/top_bar.blade.php
+++ resources/views/admin/partials/top_bar.blade.php
@@ -12,8 +12,8 @@
 
     <div class="container-fluid px-xxl-4">
         <a class="navbar-brand" href="{{ url('/') }}">
-            <img style="height: 32px; width: 74px;" src="{!! Vite::asset('resources/images/logo.png') !!}"
-                alt="{{ config('app.name', 'Laravel') }}">
+            <img style="height: 32px" src="{!! Vite::asset('resources/images/logos/logos-lt/logo-lt-horizontal.png') !!}"
+                 alt="{{ config('app.name', 'Laravel') }}">
         </a>
 
         <!-- Right Side Of Navbar -->
@@ -30,12 +30,14 @@
                 @endif
             @else
                 <li class="nav-item dropdown">
-                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                        data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#"
+                       role="button" data-bs-toggle="dropdown" aria-haspopup="true"
+                       aria-expanded="false" v-pre>
                         {{ Auth::user()->name }}
                     </a>
 
-                    <div class="dropdown-menu dropdown-menu-end dropdown-menu-dark" aria-labelledby="navbarDropdown">
+                    <div class="dropdown-menu dropdown-menu-end dropdown-menu-dark"
+                         aria-labelledby="navbarDropdown">
                         <a class="dropdown-item" href="{{ url('admin') }}">
                             Dashboard
                         </a>
@@ -43,12 +45,13 @@
                             Profile
                         </a>
                         <a class="dropdown-item" href="{{ route('logout') }}"
-                            onclick="event.preventDefault();
+                           onclick="event.preventDefault();
                                              document.getElementById('logout-form').submit();">
                             Logout
                         </a>
 
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                              class="d-none">
                             @csrf
                         </form>
                     </div>
