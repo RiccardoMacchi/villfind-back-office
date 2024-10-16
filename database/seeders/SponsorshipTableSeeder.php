59a2f204b0aedd6a00249be398d116b7d36073d7
EnxhiHoxhaj <hoxhajenxhi1@gmail.com>
2024-10-16 13:31:54 +0200

    feat(sponsorship): add seeder

--- database/seeders/SponsorshipTableSeeder.php
+++ database/seeders/SponsorshipTableSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Sponsorship;
 
 class SponsorshipTableSeeder extends Seeder
 {
@@ -12,6 +13,15 @@ class SponsorshipTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $csv_file = fopen(__DIR__ . '/../data/sponsorship.csv', 'r');
+        for ($row_count = 0; ($row_data = fgetcsv($csv_file)) != false; $row_count++) {
+            if ($row_count) {
+                $new_villain = new Sponsorship();
+                $new_villain->name = $row_data[0];
+                $new_villain->price = $row_data[1];
+                $new_villain->hours = $row_data[2];;
+                $new_villain->save();
+            }
+        }
     }
 }
