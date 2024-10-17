f8a9313305996b8a9f65a6a6107d1eb1752cb3b3
Jacopo Scorretti <jacoposcorretti.99@gmail.com>
2024-10-17 03:00:15 +0200

    feat(sponsorship): crud index completed

--- resources/views/admin/sponsorship/index.blade.php
+++ resources/views/admin/sponsorship/index.blade.php
@@ -6,23 +6,28 @@
             Sponsorship
         </h1>
 
-        <div class="d-flex justify-content-between gap-3">
+        <div class="d-flex justify-content-center gap-3 flex-wrap">
             @foreach ($sponsorship as $plan)
-                <div class="card mb-3 p-0 p-4" style="width: 15rem">
+                <div class="card p-3 d-flex flex-column justify-content-end gap-4" style="width: 15rem">
+                    <h2 class="card-title text-primary mb-auto" style="font-size: 1.75rem">
+                        {{ $plan->name }}
+                    </h2>
+
                     <div>
-                        <h3 class="card-title mb-4 fw-bold text-primary fs-5">{{ $plan->name }}</h3>
                         <div>
-                            <span class=" fs-6" style="font: 900"><strong>Price:</strong></span>
-                            <span>{{ $plan->price }} &#8364</span>
+                            <strong class="fw-bolder">Price:</strong>
+                            <span>{{ number_format($plan->price, 2) }} &#8364</span>
                         </div>
+
                         <div>
-                            <span class="fs-6"><strong>Duration:</strong></span>
+                            <strong class="fw-bolder">Duration:</strong>
                             <span>{{ $plan->hours }} hours</span>
                         </div>
                     </div>
-                    <div class="flex-shrink-0 py-3 text-primary fs-3">
+
+                    <a href="#" class="btn btn-primary btn-lg">
                         <i class="fa-solid fa-cart-plus"></i>
-                    </div>
+                    </a>
                 </div>
             @endforeach
         </div>
