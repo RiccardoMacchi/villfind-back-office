6bdbdf09d0aaa2a9e0caec424cbd8d9efdd35b98
RiccardoMacchi <rick.mac97@outlook.com>
2024-10-17 10:55:35 +0200

    feat(ratings): added review stars

--- resources/views/admin/ratings/show.blade.php
+++ resources/views/admin/ratings/show.blade.php
@@ -16,10 +16,14 @@
         <div class="fs-6 mb-5"><strong>Date:</strong> {{ $rating->created_at->format('d/m/Y H:i') }}</div>
         <div class="fs-5 mb-3">
             <strong>Vote:</strong>
+            <span class="rating">
+                {!! \App\Functions\Helper::iconifyRating($rating->value) !!}
+            </span>
         </div>
 
-        <div class="fs-5 mt-4 mb-2"><strong>Review:</strong></div>
-        <p>{{ $rating->pivot->content }}</p>
+        <div class="fs-5 mt-4 mb-2"><strong>Review:</strong>
+        </div>
+        <p>{{ $rating->pivot->content ?? 'No content' }}</p>
 
         <a href="{{ route('admin.ratings.index') }}" class="btn btn-primary mt-3">Back</a>
 
