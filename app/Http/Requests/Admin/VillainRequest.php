5a35a79b2bdc01fb3b115bd2bd6ae5cbc66f0f2c
Jacopo Scorretti <jacoposcorretti.99@gmail.com>
2024-10-22 05:43:57 +0200

    fix(villains): cv no more required

--- app/Http/Requests/Admin/VillainRequest.php
+++ app/Http/Requests/Admin/VillainRequest.php
@@ -24,7 +24,7 @@ class VillainRequest extends FormRequest
         return [
             'name' => 'required|string|min:3|max:250',
             'image' => 'nullable|image|max:4096',
-            'cv' => 'required|file|mimes:pdf|max:2048',
+            'cv' => 'nullable|file|mimes:pdf|max:2048',
             'phone' => 'nullable|string|min:8|max:25|regex:/^(?=(?:[^\d]*\d){8,15}[^\d]*$)[\d().,\s-]+$/',
             'country_code' => 'required_with:phone|string|min:2|max:4|regex:/^\+\d{1,3}$/',
             'universe_id' => 'required|exists:universes,id',
