52eb5ebb58c7fcb8ed955d5cc14f96dee398e414
Jacopo Scorretti <jacoposcorretti.99@gmail.com>
2024-10-24 22:27:20 +0200

    refactor(ratings): crud's show remade and restyled

--- resources/views/admin/ratings/show.blade.php
+++ resources/views/admin/ratings/show.blade.php
@@ -1,39 +1,57 @@
 @extends('layouts.app')
 
 @section('content')
-    {{-- controllo erroi --}}
-    @if ($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
-    <div class="container py-4">
-        <div class="fs-5 mb-3"><strong>Name:</strong> {{ $review->full_name }}</div>
-        <div class="fs-6 mb-5"><strong>Date:</strong> {{ $review->created_at }}</div>
-        <div class="fs-5 mb-3">
-            <strong>Vote:</strong>
-            <span class="rating">
-                {!! \App\Functions\Helper::iconifyRating($rating->value) !!}
-            </span>
-        </div>
+    <div class="container container mb-3">
+        <h1 class="text-primary my-4">
+            Review
+        </h1>
 
-        <div class="fs-5 mt-4 mb-2">
-            <strong>
-                Review:
-            </strong>
+
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+
+        <div class="row">
+            <div class="mb-4 col-lg-4">
+                <h2 class="text-primary">Name:</h2>
+                <strong class="fnt-size-4">
+                    {{ $rating->pivot->full_name }}
+                </strong>
+            </div>
+
+            <div class="mb-4 col-lg-4">
+                <h2 class="text-primary">Date:</h2>
+                <span class="fnt-size-4">
+                    {{ $rating->pivot->created_at }}
+                </span>
+            </div>
+
+            <div class="mb-4 col-lg-4">
+                <h2 class="text-primary">Vote:</h2>
+                <span class="fnt-size-4 rating">
+                    {!! \App\Functions\Helper::iconifyRating($rating->value) !!}
+                </span>
+            </div>
+
+            @if ($rating->pivot->content)
+                <div class="mb-4">
+                    <h2 class="text-primary">Review:</h2>
+                    <p class="fnt-size-4">
+                        {{ $rating->pivot->content }}
+                    </p>
+                </div>
+            @endif
         </div>
-        <p>
-            {{ $review->content ?? 'No content' }}
-        </p>
 
         <a href="{{ route('admin.ratings.index') }}" class="btn btn-primary mt-3">
             Back
         </a>
 
     </div>
-
 @endsection
