109db3e27051c2b6a7065d79f8b3e7b872ae2a08
Jacopo Scorretti <jacoposcorretti.99@gmail.com>
2024-10-24 22:27:46 +0200

    refactor(messages): crud's show remade and restyled

--- resources/views/admin/messages/show.blade.php
+++ resources/views/admin/messages/show.blade.php
@@ -1,29 +1,62 @@
 @extends('layouts.app')
 
 @section('content')
-    {{-- controllo erroi --}}
-    @if ($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
+    <div class="container container mb-3">
+        <h1 class="text-primary my-4">
+            Message
+        </h1>
+
+
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+
+        <div class="row">
+            <div class="mb-4">
+                <h2 class="text-primary">Sender:</h2>
+                <strong class="fnt-size-4">
+                    {{ $message->full_name }}
+                </strong>
+            </div>
+
+            <div class="mb-4 col-lg-4">
+                <h2 class="text-primary">Date:</h2>
+                <span class="fnt-size-4">
+                    {{ $message->created_at }}
+                </span>
+            </div>
+
+            <div class="mb-4 col-lg-4">
+                <h2 class="text-primary">Email:</h2>
+                <span class="fnt-size-4">
+                    {{ $message->email }}
+                </span>
+            </div>
+
+            <div class="mb-4 col-lg-4">
+                <h2 class="text-primary">Phone:</h2>
+                <span class="fnt-size-4">
+                    {{ $message->phone }}
+                </span>
+            </div>
+
+            <div class="mb-4">
+                <h2 class="text-primary">Text:</h2>
+                <p class="fnt-size-4">
+                    {{ $message->content }}
+                </p>
+            </div>
         </div>
-    @endif
 
-    <div class="container py-4">
-        <div class="fs-5 mb-3"><strong>Sender:</strong> {{ $message->full_name }}</div>
-        <div class="fs-6 mb-3"><strong>Phone:</strong> {{ $message->phone }}</div>
-        <div class="fs-6 mb-5"><strong>Email:</strong> {{ $message->email }}</div>
+        <a href="{{ route('admin.ratings.index') }}" class="btn btn-primary mt-3">
+            Back
+        </a>
 
-        <div class="fs-5 mb-3"><strong>Message:</strong></div>
-        <p>{{ $message->content }}</p>
-
-        <button class="btn btn-primary mt-4">
-            <a href="{{ route('admin.messages.index') }}">Back</a>
-        </button>
     </div>
-
-
 @endsection
