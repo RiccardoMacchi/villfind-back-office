bb0b9cd7a56caa66e4c9bd09c425c610b4ddb773
EnxhiHoxhaj <hoxhajenxhi1@gmail.com>
2024-11-02 01:19:18 +0100

    feat: side bar item order

--- config/admin.php
+++ config/admin.php
@@ -14,24 +14,24 @@ return [
         //     'menu_icon' => '<i class="fa-solid fa-book-skull"></i>',
         // ],
         [
-            'menu_text' => 'MESSAGES',
-            'route_name' => 'admin.messages.index',
-            'menu_icon' => '<i class="fa-solid fa-envelope"></i>',
-        ],
-        [
             'menu_text' => 'RATINGS',
             'route_name' => 'admin.ratings.index',
             'menu_icon' => '<i class="fa-solid fa-star-half-stroke"></i>',
         ],
         [
-            'menu_text' => 'SPONSORSHIPS',
-            'route_name' => 'admin.sponsorship.index',
-            'menu_icon' => '<i class="fa-solid fa-hand-holding-dollar"></i>',
+            'menu_text' => 'MESSAGES',
+            'route_name' => 'admin.messages.index',
+            'menu_icon' => '<i class="fa-solid fa-envelope"></i>',
         ],
         [
             'menu_text' => 'STATISTICS',
             'route_name' => 'admin.statistic.index',
             'menu_icon' => '<i class="fa-solid fa-chart-column"></i>',
+        ],
+        [
+            'menu_text' => 'SPONSORSHIPS',
+            'route_name' => 'admin.sponsorship.index',
+            'menu_icon' => '<i class="fa-solid fa-hand-holding-dollar"></i>',
         ],
     ],
 ];
